40132df31e814be1ad56c86b346fd81d2efdd0bd
Jaydeep-07 <jaydeepvpatil225@gmail.com>
2023-03-04 14:10:34 +0530

    Initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,93 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { } from 'rxjs/operators'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ObservableAndPromiseDemo';
+
+  constructor() {
+
+    // //-------------------------------- 1. Emit Values-------------------------------------
+    // var observable = new Observable(res => {
+    //   res.next("Hello Piyush");
+    //   res.next("Hello Vivek");
+    //   res.next("Hello Rajesh");
+    // });
+    // observable.subscribe(console.log)
+
+    // //Output --> Observable can emmit multiple values
+    // //Hello Piyush
+    // //Hello Vivek
+    // //Hello Rajesh
+
+    // var promise = new Promise(res => {
+    //   res("Hello Piyush");
+    //   res("Hello Vivek");
+    //   res("Hello Rajesh");
+    // });
+    // promise.then(console.log)
+
+    // //output
+    // //Hello Piyush --> Promise will emit only single values that's why other two values are not emmited.
+
+    // //------------------------------------------------------------------------------------
+
+    // //----------------------------------- 2. Lazy Load------------------------------------
+
+    // var observable = new Observable(res => {
+    //   res.next("Hello....");
+    // });
+    // observable.subscribe(console.log) //Whenever we subscribe observable then only it will emmit values beacuse it is lazy load.
+
+    // var promise = new Promise(res => {
+
+    //   console.log("Promise start executing..."); //In console you can see this output before calling promise it will initialized on creation and eagerly load.
+
+    //   res("Hello....");
+    // });
+    // promise.then(console.log)
+
+    // //-----------------------------------------------------------------------------------
+
+    //--------------------3.Asynchronous Promise Vs Synchronous/Asynchronous Observables------------------------
+
+    // //Async Promise
+    // var promise = new Promise(res => {
+    //   console.log("Start Executing...");
+
+    //   res("Hello...");
+
+    //   console.log("Execution End...");
+    // });
+    // promise.then(console.log)
+
+    // //Async Observable
+    // var observable = new Observable(res => {
+    //   console.log("Start Executing...");
+    //   setTimeout(() => {
+    //     res.next("Hello Piyush");
+    //     res.next("Hello Vivek");
+    //     res.next("Hello Rajesh");
+    //   })
+    //     , 1000
+    //   console.log("Execution End...");
+    // });
+    // observable.subscribe(console.log)
+
+    //Sync Observable
+    var observable = new Observable(res => {
+      console.log("Start Executing...");
+      res.next("Hello Piyush");
+      res.next("Hello Vivek");
+      res.next("Hello Rajesh");
+      console.log("Execution End...");
+    });
+    observable.subscribe(console.log)
+  }
+}
+
